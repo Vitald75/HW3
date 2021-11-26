05b5540ec2fd2cb2a73041af1971497851613c30
Dolgoshei Vitali <Vitald75@gmail.com>
2021-11-26 17:03:43 +0300

    hw3

--- specs/create-manager.e2e.js
+++ specs/create-manager.e2e.js
@@ -15,8 +15,10 @@ describe("Create manager", function () {
     await $("button[type='submit']").click();
   }*/
 
-  before ("Login with credentials walker@jw.com:password", async function(){
-    await browser.url("https://viktor-silakov.github.io/course-sut/index.html?quick");
+  before("Login with credentials walker@jw.com:password", async function () {
+    await browser.url(
+      "https://viktor-silakov.github.io/course-sut/index.html?quick"
+    );
     await $("#login").setValue("walker@jw.com");
     await $("#password").setValue("password");
     await $("button").click();
@@ -24,15 +26,15 @@ describe("Create manager", function () {
       reverse: true,
       timeout: 5000,
     });
-  })
+  });
 
-  it("should open Create manager form, fill from Json string", async function () {
+  it("should open Create manager form, fill fields ", async function () {
     const titlePortal = await browser.getTitle();
     if (titlePortal !== "Report portal") {
       throw new Error("You don`t login into system!!!");
     }
     await $("a[href='./formManager.html']").click();
-    
+
     const titleUserForm = await browser.getTitle();
     if (titleUserForm !== "User form") {
       throw new Error("You don`t open User form page!!!");
@@ -42,35 +44,33 @@ describe("Create manager", function () {
     await $("#password").addValue("1452qw");
     await $("#address1").addValue("addr1");
     await $("#address2").addValue("addr2");
-    await $("#state").addValue("state of ");
+    //await $("#state").addValue("state of ");
+    const state = await $("#state");
+    await state.click();
+    await state.selectByVisibleText("India");
+
     await $("#zip").addValue("220000");
     await $("#description").addValue("some descriptions");
-    
+
     await $("#demo-balance").click();
     await $("#wait-supervisor").click();
     await $("input[value='country']").click();
+
     await $("input[name='city']").addValue("Montreal");
 
     const list = await $("#autoComplete_list_1");
-    console.log(list.getValue());
-   
-    await list.selectByVisibleText('0 USA');
+    //console.log(list.getValue());
+    await $("#autoComplete_result_1").click();
+
+    await browser.pause(5000);
 
     //await list.selectByIndex(3);
     //const result = await list.getValue();
     //await $("input[name='city']").setValue(result);
-    
-
-    //await radiocheck.checked = true ;
-    //await radiocheck.selectByVisibleText('Demo balance');
 
     //await $("#wait-supervisor").se
-    await browser.pause(5000);
-    //await $("button[type='submit']").click();
+    await $("button[type='submit']").click();
 
-    //const stringJson = '{ "email" : "email.com", "password" : "Passw", "address1" : " ffs", "address2" : " аворпорорва ", "city" : " Minsk", "zip": "220000", "description": "fdkuhjhf kh fksjhkjfhse k"}';
-    //const dataJson = JSON.parse(stringJson);
-    //await fillFormUsingJson(dataJson);
     await browser.pause(3000);
   });
 });
